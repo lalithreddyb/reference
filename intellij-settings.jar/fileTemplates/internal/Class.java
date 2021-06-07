3d52cd25c7519f963837b1048252cd650cf1a5de
lalithreddyb <44688085+lalithreddyb@users.noreply.github.com>
2021-06-06 20:27:11 -0400

    Create Class.java

---
+++ intellij-settings.jar/fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2017 Fannie Mae. All rights reserved.
+ *
+ * This software contains confidential information and trade secrets of Fannie Mae.
+ * Use, disclosure, or reproduction is prohibited without the prior written consent of Fannie Mae.
+*/
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
